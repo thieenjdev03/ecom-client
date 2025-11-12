61b087ee76e1d4ec6cba4a2a63bddb0abf6ecb3a
thiendev <thien@gkim.vn>
2025-11-12 17:10:23 +0700

    fix: update themeColorPresets to 'cyan' in RootLayout for improved visual consistency

--- src/app/homepage/layout.tsx
+++ src/app/homepage/layout.tsx
@@ -75,7 +75,7 @@ export default function RootLayout({ children }: Props) {
                 themeDirection: "ltr", //  'rtl' | 'ltr'
                 themeContrast: "default", // 'default' | 'bold'
                 themeLayout: "vertical", // 'vertical' | 'horizontal' | 'mini'
-                themeColorPresets: "default", // 'default' | 'cyan' | 'purple' | 'blue' | 'orange' | 'red'
+                themeColorPresets: "cyan", // 'default' | 'cyan' | 'purple' | 'blue' | 'orange' | 'red'
                 themeStretch: false,
               }}
             >
